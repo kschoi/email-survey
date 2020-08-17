c2cc9346b21798022d5054c2e8f8c2c532f84ea2
kyungsun choi <kschoi0308@gmail.com>
2020-08-17 18:27:42 +0900

    add env

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-app.get("/", (req, res) => {
-	res.send({ hi: "there" });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 
