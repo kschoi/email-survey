7cf5761a51d64493fc89e5105310cafa327e9bf0
kyungsun choi <kschoi0308@gmail.com>
2020-08-17 17:52:27 +0900

    add dotenv

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+	res.send({ hi: "there" });
 });
 
 const PORT = process.env.PORT || 5000;
