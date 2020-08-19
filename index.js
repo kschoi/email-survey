a482b05fc78b13b0db1aafd7a08d43aba2847be5
kyungsun choi <kschoi0308@gmail.com>
2020-08-19 09:20:08 +0900

    refactoring

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 const express = require("express");
 const app = express();
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const dotenv = require("dotenv");
-
 dotenv.config();
+
+require("./services/passport");
 
 const PORT = process.env.PORT || 5000;
 let isDisableKeepAlive = false;
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken) => {
-      console.log(acessToken);
-    }
-  )
-);
 
 app.use((req, res, next) => {
   if (isDisableKeepAlive) {
@@ -29,19 +15,10 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
-);
+require("./routes/routes")(app);
 
 app.listen(PORT, () => {
-  // process.send("ready");
+  process.send && process.send("ready");
   console.log(`application is listening on port ${PORT}...`);
 });
 
